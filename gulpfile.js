5ffbc65624936b20ba3c626c44c8b03ad821d9b6
João Victor Diniz <joaodnz@proton.me>
2024-03-27 11:24:50 -0300

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+
+const { src, task, dest, watch, parallel } = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    path = require('path'),
+    sourceMaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin');
+
+// você pode usar o prefixo '_' para arquivos scss que não deseja compilar em sua build
+
+const sassPath = path.join(__dirname, 'src/scss/*.scss'),
+    cssPath = path.join(__dirname, 'build/css/main.css'),
+    jsPath = path.join(__dirname, 'src/scripts/*.js'),
+    jsBuildPath = path.join(__dirname, 'build/scripts'),
+    imgPath = path.join(__dirname, 'src/img'),
+    imgPathBuild = path.join(__dirname, 'build/img');
+task('sassCompiler', () => {
+    return src(sassPath)
+        .pipe(sourceMaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed',
+        }))
+        .pipe(sourceMaps.write('./maps'))
+        .pipe(dest(cssPath));
+
+});
+
+task('minifyJs', () => {
+    return src(jsPath)
+        .pipe(uglify())
+        .pipe(dest(jsBuildPath));
+
+});
+
+task('imagemin', () => {
+    return src(imgPath)
+        .pipe(imagemin())
+        .pipe(dest(imgPathBuild));
+});
+task('watch', () => {
+    watch(sassPath, parallel('sassCompiler'));
+    watch(jsPath, parallel('minifyJs'));
+    watch(imgPath, parallel('imagemin'));
+});
+
+task('default', parallel('sassCompiler', 'minifyJs', 'imagemin', 'watch'));
